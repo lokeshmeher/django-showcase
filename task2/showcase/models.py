fbf291a93db0b6e93336b7e3345903381f929ec9
Lokesh Meher <lokeshmeher34@gmail.com>
2016-07-22 17:08:55 +0530

    Add basic project structure

--- task2/showcase/models.py
+++ task2/showcase/models.py
@@ -3,7 +3,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 
+class Tag(models.Model):
+	name = models.CharField(max_length=30)
+
+
 class Product(models.Model):
 	title = models.CharField(max_length=50)
+	rating = models.PositiveIntegerField(default=0)
 	description = models.TextField()
-	tags = models.ManyToManyField()
+	tags = models.ManyToManyField(Tag)
--- task2/showcase/views.py
+++ task2/showcase/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+
+class Home(ListView):
+    pass
+
+
+class ProductDetail(DetailView):
+    pass
