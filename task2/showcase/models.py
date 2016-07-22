0d9ff799558be0b016352108d4da06af64899d3d
Lokesh <lokeshmeher34@gmail.com>
2016-07-22 13:36:21 +0530

    Add model Product

--- task2/showcase/models.py
+++ task2/showcase/models.py
@@ -2,4 +2,8 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+	title = models.CharField(max_length=50)
+	description = models.TextField()
+	tags = models.ManyToManyField()
